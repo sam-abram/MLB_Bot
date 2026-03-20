0e650db30aeab8d93a8e3468c307faf1f27d926c
sam-abram <sam.abramowicz@gmail.com>
2026-03-19 22:34:19 -0400

    Remove non-production files and old artifacts

--- test_bot.py
+++
@@ -1,8 +0,0 @@
-from pybaseball import statcast
-
-# This pulls a tiny amount of data (one day) just to test the connection
-print("Attempting to download data...")
-data = statcast(start_dt='2024-06-01', end_dt='2024-06-01')
-
-print("Success! Here is the data:")
-print(data.head())
